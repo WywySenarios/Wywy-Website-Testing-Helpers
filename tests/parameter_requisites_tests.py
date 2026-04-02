d8552c0c19081f514bdcdd5592118fb818e8cfbd
WywySenarios <wywyericzhu@gmail.com>
2026-04-01 22:35:21 -0400

    Add negative_test_endpoint_paramters
    Consolidates a LOT of boilerplate code involving authentication testing, endpoint validity, etc.

---
+++ tests/parameter_requisites_tests.py
@@ -0,0 +1,56 @@
+from unittest import TestCase
+from string import Template
+from requests import get as GET, post as POST
+from typing import Literal, Any
+
+
+def negative_test_endpoint_parameters(
+    test_object: TestCase,
+    endpoint_template: Template,
+    required_parameters: dict[str, str],
+    request_method: Literal["GET", "POST"],
+    request_params: dict[str, Any],
+) -> None:
+    """Verifies that all required parameters are indeed required.
+
+    Args:
+        test_object (TestCase): The test object to use.
+        endpoint_template (Template): The endpoint template to validate.
+        required_parameters (dict[str, str]): Valid required templates that would normally produce a 200 result or similar.
+        request_method (Literal[&quot;GET&quot;, &quot;POST&quot;]): The request method to test with (GET or POST).
+        request_params (dict[str, Any]): Valid request parameters that would normally produce a 200 result or similar.
+    """
+
+    for required_parameter in required_parameters:
+        params = {**required_parameters}
+        del params[required_parameter]
+        endpoint = endpoint_template.substitute(params)
+
+        match (request_method):
+            case "GET":
+                response = GET(endpoint, **request_params)
+            case "POST":
+                response = POST(endpoint, **request_params)
+
+        test_object.assertEqual(
+            response.status_code,
+            400,
+            f"Invalid endpoint access (missing required parameter). Expected status 400, received {response.status_code}: {response.text}.",
+        )
+
+    for required_parameter in required_parameters:
+        params = {**required_parameters}
+        params[required_parameter] = "this string will probably never be valid"
+        endpoint = endpoint_template.substitute(params)
+
+        match (request_method):
+            case "GET":
+                response = GET(endpoint, **request_params)
+            case "POST":
+                response = POST(endpoint, **request_params)
+
+        test_object.assertEqual(
+            response.status_code,
+            400,
+            f"Invalid endpoint access (invalid required parameter). Expected status 400, received {response.status_code}: {response.text}.",
+        )
